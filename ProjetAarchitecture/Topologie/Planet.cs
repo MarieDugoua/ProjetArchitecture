091049b9b3deb83bff3fdae7796008f381097760
Lea <lealoustau.pro@gmail.com>
2023-11-03 14:49:55 +0100

    constructeur planet

--- ProjetAarchitecture/Topologie/Planet.cs
+++ ProjetAarchitecture/Topologie/Planet.cs
@@ -8,5 +8,21 @@ namespace ProjetAarchitecture.Topologie
 {
     public class Planet
     {
+        public int Width { get; }
+        public int Height { get; }
+        private readonly List<Obstacle> _obstacles;
+
+        public Planet(int width, int height, IEnumerable<Obstacle> obstacles = null)
+        {
+            Width = width;
+            Height = height;
+            _obstacles = obstacles?.ToList() ?? new List<Obstacle>();
+        }
+        public Position AdjustPosition(Position position)
+        {
+            int adjustedX = (position.X + Width) % Width;
+            int adjustedY = (position.Y + Height) % Height;
+            return new Position(adjustedX, adjustedY);
+        }
     }
 }
