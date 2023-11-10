e4a6d9c6c6e00af87a8816cd6a81c4baae4634d9
Jacques HU <jacques33185@gmail.com>
2023-11-10 12:28:53 +0100

    restablish missing files

---
+++ ProjetArchitecture/MarsRover/MainRover.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjetArchitecture.MarsRover;
+using ProjetArchitecture.Command;
+
+public class MainRover
+{
+    public MainRover() { }
+    public void ExecuteCommand(Command command, Rover rover) {
+        rover.ExecuteCommand(command);
+    }    
+
+    public string returnPosition(Rover rover)
+    {
+        return rover.ToString();
+    }
+}
---
+++ ProjetArchitecture/README.md
@@ -0,0 +1,10 @@
+# ProjetArchitecture
+Equipe : Omer Yener, Jacques HU, Marie Dugoua, Hugo Piaï, Léa Loustau, Romain Michel. 
+Langage : C#
+
+
+
+Diagramme V1
+
+
+![diagramme_V1](./ressources/diagramme_V1.png)
