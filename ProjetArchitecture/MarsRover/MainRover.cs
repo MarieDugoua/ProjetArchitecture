755952e6381cf769c26aaf8c1285ca271000f953
MarieDugoua <mariedugoua@gmail.com>
2023-11-23 13:06:06 +0100

    edit name

---
+++ ProjetArchitecture/MarsRover/MainRover.cs
@@ -0,0 +1,22 @@
+﻿using ProjetAarchitecture.Commandes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetAarchitecture.MarsRover
+{
+    public class MainRover
+    {
+        public MainRover() { }
+        public void ExecuteCommand(Commands command, Rover rover) {
+            rover.ExecuteCommand(command);
+        }    
+
+        public string returnPosition(Rover rover)
+        {
+            return rover.ToString();
+        }
+    }
+}
---
+++ ProjetArchitecture/Topologie/Orientation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetAarchitecture.Topologie
+{
+    public enum Orientation
+    {
+        N, // North
+        E, // East
+        S, // South
+        W  // West
+    }
+}
