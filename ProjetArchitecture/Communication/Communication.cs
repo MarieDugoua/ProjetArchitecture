e7a5b215cca6e9ef58c1e1e4eb1af9d9f5742ed9
MarieDugoua <mariedugoua@gmail.com>
2023-11-23 13:23:51 +0100

    edit comm

--- ProjetArchitecture/Communication/Communication.cs
+++ ProjetArchitecture/Communication/Communication.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ProjetAarchitecture.Communication;
 
-namespace ProjetAarchitecture.Communication
+internal class Communication
 {
-    internal class Communication
-    {
-    }
 }
+
